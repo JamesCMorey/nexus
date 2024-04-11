ceb67d1f20f44fa6d3dfec5c2cceb8bf9ab40d89
JamesCMorey <jmorey@tutanota.com>
2024-04-10 21:59:11 -0500

    Reworking tabbing system currently semi-functional
    This code compiles, runs, and is able to create a new tab with the
    connection, but the tabbing system isn't fully functional. Still
    implementing text storage system for each tab and tab-switching
    functionality.

--- display.c
+++ display.c
@@ -8,11 +8,19 @@
 #define CLEAR_WIN(win) werase(win); box(win, 0, 0)
 #define REMOVE_LAST_CHAR(arr) arr[strlen(arr) - 1] = '\0'
 
+#define MAX_TABNAME_LEN 10
+#define MSGS_PER_TAB 16384
+#define MAX_MSG_LEN 500
+
 struct tab {
-	char tabname[10]; /* Name that is displayed in the nav bar for the tab */
+	/* Name that is displayed in the nav bar for the tab */
+	char tabname[MAX_TABNAME_LEN];
+	int id;
 	int unread:1; /* Flag for unseen activity in tab */
-	int id; /* 0 means local/not networked */
-	WINDOW *win;
+
+	char msgs[MSGS_PER_TAB][MAX_MSG_LEN];
+	int msgnum;
+
 	int y, x;
 };
 
@@ -27,26 +35,31 @@ struct screenState {
 	int iy, ix;
 	char buffer[1024]; /* input buffer from WINDOW *input */
 
+	WINDOW *display;
+	int max_dy, max_dx;
+	int darea;
+
 	/* tabs organized with file descriptor such that tabs[sfd]; is the tab
 	 * that is used to display communications to and from that socket. */
 	struct tab *tabs[1025]; // tabs[0] is the default
 	struct tab *curtab;
-	WINDOW *curwin;
 	int maxtab;
 };
 
 /* global struct to hold all information, windows, and input from the screen */
-struct screenState *Screen;
+static struct screenState *Screen;
 
 static void clrwin(WINDOW *win);
 static void deltab(struct tab *tb);
 static void show_tabs(void);
-static void add_to_tab(char *buffer);
-void store_curwin();
-static WINDOW *retrieve_win(int id);
+static void display_curtab();
+static void println(char *text);
+static void addmsg(struct tab *tb, int type, char *text, const void *arg);
+static void display_tab(struct tab *tb);
+static int nummsg_display();
 
 /* INPUT */
-int handle_input()
+int handle_input() /* TODO add check to ensure input is <500 chars */
 {
 	int rv = 0;
 	char c;
@@ -64,7 +77,9 @@ int handle_input()
 	/* Normal text handling */
 	else if (c == '\n') {
 		REMOVE_LAST_CHAR(Screen->buffer);
+		wlog("Displaying text...");
 		display(STR, "%s", Screen->buffer);
+		wlog("Displayed text.");
 
 		memset(Screen->buffer, 0, sizeof(Screen->buffer));
 	}
@@ -88,35 +103,80 @@ int handle_input()
 }
 
 /* MISC */
+
 void display(int type, char *text, const void *arg)
 {
 	switch(type) {
 	case NOARG:
-		mvwprintw(Screen->curwin, ++Screen->curtab->y, 1, "%s", text);
+		mvwprintw(Screen->display, ++Screen->curtab->y, 1, "%s", text);
 		break;
 
 	case INT:
-		mvwprintw(Screen->curwin, ++Screen->curtab->y, 1, text,
-							*(int *)arg);
+		mvwprintw(Screen->display, ++Screen->curtab->y, 1, text,
+								*(int *)arg);
 		break;
 
 	case STR:
-		mvwprintw(Screen->curwin, ++Screen->curtab->y, 1, text,
-							(char *)arg);
+		walog(INT, "Printing at y=%d...", &Screen->curtab->y);
+		mvwprintw(Screen->display, ++Screen->curtab->y, 1, text,
+								(char *)arg);
 		break;
 
 	default:
-		mvwprintw(Screen->curwin, ++Screen->curtab->y, 1,
+		mvwprintw(Screen->display, ++Screen->curtab->y, 1,
 			"Type not supported in call to display(): %s, %d",
 			__FILE__, __LINE__);
 	}
 
-	wrefresh(Screen->curwin);
+	wlog("Printed.");
+	/*
+	clr_display();
+	addmsg(Screen->curtab, type, text, arg);
+	display_tab(Screen->curtab);
+	*/
+	wrefresh(Screen->display);
 }
 
-void clr_cur_win()
+static void display_tab(struct tab *tb)
 {
-	clrwin(Screen->curwin);
+
+}
+
+static int nummsg_display()
+{
+	int rows, msgnum, msgindex, size;
+	int extra;
+
+	while (rows < Screen->max_dy) {
+		/* count numrows the msg takes */
+		msgindex = Screen->curtab->msgnum - msgnum - 1;
+		size = strlen(Screen->curtab->msgs[msgindex]);
+
+		/* only if the screen can hold the whole message */
+		if (size == 0) {
+			msgnum++;
+		}
+	}
+
+}
+
+void printmsg(char *text)
+{
+	int len;
+	while (len > 0) {
+		len -= (Screen->max_dx - 2);
+	}
+}
+
+static void println(char *text)
+{
+	mvwprintw(Screen->display, ++Screen->curtab->y, 1, "%s", text);
+	wrefresh(Screen->display);
+}
+
+void clr_display(void)
+{
+	clrwin(Screen->display);
 }
 
 static void clrwin(WINDOW *win)
@@ -127,31 +187,75 @@ static void clrwin(WINDOW *win)
 }
 
 /* ===== TABBING ====== */
+
+static void addmsg(struct tab *tb, int type, char *text, const void *arg)
+{
+	switch(type) {
+	case NOARG:
+		snprintf(tb->msgs[tb->msgnum], MAX_MSG_LEN, "%s", text);
+		break;
+
+	case INT:
+		snprintf(tb->msgs[tb->msgnum], MAX_MSG_LEN, text, *(int *)arg);
+		break;
+
+	case STR:
+		snprintf(tb->msgs[tb->msgnum], MAX_MSG_LEN, text, *(char *)arg);
+		break;
+
+	default:
+		snprintf(tb->msgs[tb->msgnum], MAX_MSG_LEN,
+			"Type not supported in call to display(): %s, %d",
+			__FILE__, __LINE__);
+	}
+
+	strncpy(tb->msgs[tb->msgnum], text, sizeof(text));
+	tb->msgnum++;
+}
+
+/* create a tab and set it to Screen->curtab */
 void mktab(char *name, int id)
 {
-	int i = id;
+	int i;
+	if (id > 3) { /* Make up for default fds stdin, stdout, stderr */
+		i = id - 3;
+	}
+	else {
+		i = id;
+	}
 
 	while (Screen->tabs[i] != NULL) {
 		i++;
 	}
+
+
 	Screen->tabs[i] = malloc(sizeof(struct tab));
 	strncpy(Screen->tabs[i]->tabname, name, sizeof(char) * 10);
-	Screen->tabs[i]->win = newwin(Screen->rows-2, 4*Screen->rows-2, 0,
-								Screen->cols/5);
 	Screen->tabs[i]->id = id; /* Allows matching network conn to tab */
+	Screen->tabs[i]->y = 0;
+	Screen->tabs[i]->x = 0;
+	Screen->tabs[i]->msgnum = 0;
 
+	// getyx(Screen->display, Screen->curtab->y, Screen->curtab->x);
+
+	Screen->curtab = Screen->tabs[i];
 	if (i > Screen->maxtab) {
 		Screen->maxtab = i;
 	}
-	clrwin(Screen->tabs[i]->win);
-	switch_tab(id);
-	getyx(Screen->curwin, Screen->curtab->y, Screen->curtab->x);
+
+	display_curtab();
+
 	show_tabs();
 }
 
+static void display_curtab()
+{
+	//display();
+}
+
+/* In case more things are added to struct tab */
 static void deltab(struct tab *tb)
 {
-	delwin(tb->win);
 	free(tb);
 }
 
@@ -193,52 +297,19 @@ static void show_tabs()
 void switch_tab(int id)
 {
 	for (int i = id; i <= Screen->maxtab; i++) {
-		display(INT, "i: %d", &i);
-		if (Screen->tabs[i] != NULL && Screen->tabs[i]->id == id) {
-			clrwin(Screen->curtab->win);
-
+		if (Screen->tabs[i]->id == id) {
 			Screen->curtab = Screen->tabs[i];
-			Screen->curwin = Screen->tabs[i]->win;
-
-			display(INT, "Switching to %d.", &Screen->tabs[i]->id);
-			wrefresh(Screen->curwin);
-			show_tabs();
-			return;
+			display_curtab();
 		}
 	}
-
-	display(INT, "Unable to find tab of id: %d", &id);
 }
 
-void store_curwin()
+/*
+void quiet_display()
 {
-	char filename[10];
-	char mode = 'w';
-	FILE *fd;
-
-	sprintf(filename, ".%d.win", Screen->curtab->id);
-	fd = fopen(filename, &mode);
-
-	putwin(Screen->curtab->win, fd);
-	clrwin(Screen->curtab->win);
-	//delwin(Screen->curtab->win);
 }
+*/
 
-static WINDOW *retrieve_win(int id)
-{
-	char filename[10];
-	char mode = 'w';
-	FILE *fd;
-
-	sprintf(filename, ".%d.win", id);
-	fd = fopen(filename, &mode);
-
-	return getwin(fd);
-}
-
-static void add_to_tab(char *buffer) {
-
-}
 
 /* ====== INIT AND CLEANUP ====== */
 void init_screen(void)
@@ -258,46 +329,37 @@ void init_screen(void)
 	int cols = Screen->cols;
 	int rows = Screen->rows;
 
-	// windows
+	/* windows and default tab */
 	Screen->nav = newwin(rows, cols/5, 0, 0);
 	Screen->input = newwin(3, 4*cols/5, rows-3, cols/5);
+	Screen->display = newwin(rows-2, 4*rows-2, 0, cols/5);
 
-	wlog("Windows created...");
+	mktab("default", 0);
+	wlog("Windows and default tab initialized...");
 
-	/* Creating tab around Screen->display and setting that to tabs[0] */
-	Screen->tabs[0] = malloc(sizeof(struct tab));
-	strncpy(Screen->tabs[0]->tabname, "default", sizeof(char) * 10);
-	Screen->tabs[0]->win = newwin(rows-2, 4*rows-2, 0, cols/5);
-	Screen->curwin = Screen->tabs[0]->win;
+	/* window info */
+	getyx(Screen->nav, Screen->ny, Screen->nx);
+	getmaxyx(Screen->nav, Screen->max_ny, Screen->max_nx);
 
-	/* Set display to current visible win */
-	Screen->curtab = Screen->tabs[0];
+	getyx(Screen->display, Screen->curtab->y, Screen->curtab->x);
+	getmaxyx(Screen->display, Screen->max_dy, Screen->max_dx);
+	Screen->darea = Screen->max_dy * Screen->max_dy;
 
-	wlog("Tabs allocated...");
+	wlog("Screen struct variables initialized...");
 
-	refresh(); // don't know why this is necessary here
-
-	// creating outlines for main windows
+	/* Displaying everything */
 	box(Screen->nav, 0, 0);
 	wrefresh(Screen->nav);
 
-	box(Screen->curwin, 0, 0);
-	wrefresh(Screen->curwin);
+	box(Screen->display, 0, 0);
+	wrefresh(Screen->display);
 
 	box(Screen->input, 0, 0);
 	wmove(Screen->input, 1, 1);
 	wrefresh(Screen->input);
 
-	// initializing more variables (related to created wins now though)
-	getyx(Screen->curwin, Screen->curtab->y, Screen->curtab->x);
-	getyx(Screen->nav, Screen->ny, Screen->nx);
-	getmaxyx(Screen->nav, Screen->max_ny, Screen->max_nx);
-
-	/* needs to be after Screen->max_nx is defined */
-	show_tabs();
-
-	/* This has no functional use, just makes things look nice */
 	wmove(Screen->input, 1, 1);
+	show_tabs();
 
 	wlog("Screen initialization complete.");
 }
